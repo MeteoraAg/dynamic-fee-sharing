61fb35dc37e46b70babc1bfd722d7862b9c87ce9
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-10-13 18:22:40 +0700

    validate token vault

--- programs/dynamic-fee-sharing/src/constants.rs
+++ programs/dynamic-fee-sharing/src/constants.rs
@@ -10,35 +10,44 @@ pub mod seeds {
     pub const TOKEN_VAULT_PREFIX: &[u8] = b"token_vault";
 }
 
-pub static WHITELISTED_ACTIONS: [(Pubkey, &[u8]); 7] = [
+// (program_id, instruction, index_of_token_vault_account)
+// TODO should find a way to avoid hardcoding index of token_vault_account
+pub static WHITELISTED_ACTIONS: [(Pubkey, &[u8], usize); 7] = [
     // damm v2
     (
         damm_v2::ID,
         damm_v2::client::args::ClaimPositionFee::DISCRIMINATOR,
+        4,
     ),
     (
         damm_v2::ID,
         damm_v2::client::args::ClaimReward::DISCRIMINATOR,
+        5,
     ),
     // DBC
     (
         dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::CreatorWithdrawSurplus::DISCRIMINATOR,
+        3,
     ),
     (
         dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::ClaimCreatorTradingFee::DISCRIMINATOR,
+        3,
     ),
     (
         dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::PartnerWithdrawSurplus::DISCRIMINATOR,
+        3,
     ),
     (
         dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::ClaimTradingFee::DISCRIMINATOR,
+        4,
     ),
     (
         dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::WithdrawMigrationFee::DISCRIMINATOR,
+        3,
     ),
 ];
