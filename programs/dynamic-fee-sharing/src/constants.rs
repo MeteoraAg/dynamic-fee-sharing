0d087f01fefefe7bc7720bc50c46f162f9cc22d4
Andrew Nguyen <cuong.hv.dtvt@gmail.com>
2025-10-13 13:21:49 +0700

    remove hardcode program id

--- programs/dynamic-fee-sharing/src/constants.rs
+++ programs/dynamic-fee-sharing/src/constants.rs
@@ -1,5 +1,5 @@
+use anchor_lang::prelude::Pubkey;
 use anchor_lang::Discriminator;
-use anchor_lang::{prelude::Pubkey, pubkey};
 
 pub const MAX_USER: usize = 5;
 pub const PRECISION_SCALE: u8 = 64;
@@ -10,35 +10,35 @@ pub mod seeds {
     pub const TOKEN_VAULT_PREFIX: &[u8] = b"token_vault";
 }
 
-pub const DAMM_V2: Pubkey = pubkey!("cpamdpZCGKUy5JxQXB4dcpGPiikHawvSWAd6mEn1sGG");
-pub const DBC: Pubkey = pubkey!("dbcij3LWUppWqq96dh6gJWwBifmcGfLSB5D4DuSMaqN");
-
-pub const WHITELISTED_ACTIONS: [(Pubkey, &[u8]); 7] = [
+pub static WHITELISTED_ACTIONS: [(Pubkey, &[u8]); 7] = [
     // damm v2
     (
-        DAMM_V2,
+        damm_v2::ID,
         damm_v2::client::args::ClaimPositionFee::DISCRIMINATOR,
     ),
-    (DAMM_V2, damm_v2::client::args::ClaimReward::DISCRIMINATOR),
+    (
+        damm_v2::ID,
+        damm_v2::client::args::ClaimReward::DISCRIMINATOR,
+    ),
     // DBC
     (
-        DBC,
+        dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::CreatorWithdrawSurplus::DISCRIMINATOR,
     ),
     (
-        DBC,
+        dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::ClaimCreatorTradingFee::DISCRIMINATOR,
     ),
     (
-        DBC,
+        dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::PartnerWithdrawSurplus::DISCRIMINATOR,
     ),
     (
-        DBC,
+        dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::ClaimTradingFee::DISCRIMINATOR,
     ),
     (
-        DBC,
+        dynamic_bonding_curve::ID,
         dynamic_bonding_curve::client::args::WithdrawMigrationFee::DISCRIMINATOR,
     ),
 ];
